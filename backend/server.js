c9fcdb141866fc3714fc2595ec85727987b6fe87
Atuma David <atumadavid35@gmail.com>
2024-11-08 03:06:32 +0100

    authentication(refresh-token and access token)

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectionDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows you to parse the body of the request.
+app.use(cookieParser());
 
 // auth
 app.use("/api/auth", authRoutes);
